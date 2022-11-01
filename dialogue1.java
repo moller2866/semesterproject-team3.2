739f2cb835e32d59ab28f4f51181643e365a436f
Zubeyr19 <112621028+Zubeyr19@users.noreply.github.com>
2022-11-01 12:19:55 +0100

    Add files via upload

---
+++ dialogue1.java
@@ -0,0 +1,41 @@
+public class dialogue {
+    String word;
+
+    public void getRoom() {
+
+        if (room = ocean)
+            System.out.println("You have entered ocean");
+
+    }
+
+    public void getRoom() {
+        if (room = ship)
+            System.out.println("You have entered ship");
+
+    }
+
+    public void getRoom() {
+        if (room = harbor)
+            System.out.println("You have entered harbor");
+
+
+    }
+
+    public void getRoom() {
+        if (room = thriftshop)
+            System.out.println("You have entered thrift shop");
+
+    }
+
+    public void getRoom() {
+        if (room = wheelhouse)
+            System.out.println("You have entered wheelhouse");
+
+    }
+    public void npc() {
+        f npc = true;
+        System.out.println("Well hello there");
+                            else System.out.println("You are all alone");
+    }
+
+}
