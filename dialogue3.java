f11ecaa08c0797b5294db8e2c73e4775f28c5344
Zubeyr19 <112621028+Zubeyr19@users.noreply.github.com>
2022-11-02 12:41:55 +0100

    Add files via upload

---
+++ dialogue3.java
@@ -0,0 +1,10 @@
+public class dialogue3 {
+
+    public static String dialogueRoom(boolean dialogue) {
+        if (dialogue) {
+            return "Well, hello there";
+        } else return "You are alone";
+    }
+}
+
+
