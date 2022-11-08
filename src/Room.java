bc1196935373466b54d548f40ac991dd1bcf69cb
moller2866 <kaspermoeller.t@gmail.com>
2022-11-08 09:31:54 +0100

    extended room description

--- src/Room.java
+++ src/Room.java
@@ -33,7 +33,7 @@ public class Room
 
     private String getExitString()
     {
-        String returnString = "Exits:";
+        String returnString = "\nExits:";
         Set<String> keys = exits.keySet();
         for(String exit : keys) {
             returnString += " " + exit;
