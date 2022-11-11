0a976c670c54bdc260c2f98f6dfc9abab804c98e
SaAli22 <116625689+SaAli22@users.noreply.github.com>
2022-11-11 12:02:57 +0100

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,23 +1,50 @@
 package oceanCleanup.src;
 
+import java.util.ArrayList;
+
 public class Inventory {
 
+
     Item[] inventory;
+    ArrayList<String> Items = new ArrayList<String>();
 
-    public Item[] getInventory() {
-        return null;
-    }
 
-    public String showInventory() {
-        return null;
-    }
-
-    public void addItem () {
+    public ArrayList<String> getInventory(){
+        return Items;
 
     }
 
-    public Item remove() {
-        return null;
+
+
+    public void addItem (Item item) {
+            Items.add("BUCKET");
+
+        }
+
+
+
+
+    public Item remove(Item Item) {
+        return Item;
+
+
+
+        }
+
+        public String toString(){
+        return "bom";
+        }
+
+
+
+
+
+
+// test
+    public static void main (String [] args){
+        Inventory K = new Inventory();
+        System.out.print(K.getInventory());
+
     }
 
 }
