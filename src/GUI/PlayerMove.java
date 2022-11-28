f2785d2f887d21aa1df5c6e5c1b0c18bc3837700
jlars22 <jlars22@student.sdu.dk>
2022-11-28 11:28:33 +0100

    fixed some hardcoding

--- src/GUI/PlayerMove.java
+++ src/GUI/PlayerMove.java
@@ -25,9 +25,10 @@ public class PlayerMove {
 
 
     private ImageView image;
+    final private double playerHeight = 65;
+    final private double playerWidth= 45;
+
     private AnchorPane scene;
-    final private double SCENEHEIGHT = 335;
-    final private double SCENEWIDTH = 555;
 
     public void makeMovable(ImageView image, AnchorPane scene) {
         this.image = image;
@@ -123,8 +124,8 @@ public class PlayerMove {
 
     public void playerAtBorder() {
         double leftBorder = -20;
-        double rightBorder = SCENEWIDTH;
-        double bottomBorder = SCENEHEIGHT;
+        double rightBorder = scene.getWidth()-playerWidth;
+        double bottomBorder = scene.getHeight()-playerHeight;
         double topBorder = -20;
 
         if (image.getLayoutX() <= leftBorder) {
