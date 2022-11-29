497321ac6760dcc142c0c13b4fcdf3531399e27f
jlars22 <jlars22@student.sdu.dk>
2022-11-29 10:00:10 +0100

    added dialog box, with help, talk and inventory

--- src/GUI/GameController.java
+++ src/GUI/GameController.java
@@ -2,19 +2,24 @@ package oceanCleanup.src.GUI;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import oceanCleanup.src.domain.Game;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class GameController extends Game implements Initializable {
+public class GameController implements Initializable {
+
+    Game game = new Game();
 
     @FXML
     PlayerMove player = new PlayerMove();
@@ -22,14 +27,21 @@ public class GameController extends Game implements Initializable {
     AnchorPane scene;
     @FXML
     ImageView image;
-
     @FXML
     ImageView bucket;
+    @FXML
+    private ImageView dialogbox;
+    @FXML
+    private TextArea textBox;
 
     @FXML
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         player.makeMovable(image, scene);
+        textBox.setEditable(false);
+        textBox.setMouseTransparent(true);
+        textBox.setText(game.getRoomDescription());
+        textBox.setFont(Font.font("Verdana", FontWeight.BOLD, 9));
     }
 
     @FXML
@@ -50,6 +62,19 @@ public class GameController extends Game implements Initializable {
                 || (event.getCode() == KeyCode.D)) {
             player.onKeyReleasedMovement(event.getCode());
         }
+
+        if ((event.getCode() == KeyCode.H)) {
+            textBox.setText(game.getRoomDescription());
+        }
+
+        if ((event.getCode() == KeyCode.I)) {
+            textBox.setText(game.seeInventory());
+        }
+
+        if ((event.getCode() == KeyCode.T)) {
+            textBox.setText(game.startTalk());
+        }
+
     }
 
 }
